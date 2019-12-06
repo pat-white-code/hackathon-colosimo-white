7bcb454af497cf30e1c96f3e611b15ac15185b46
pat-white-code <pat.k.white@gmail.com>
2019-12-06 15:09:34 -0600

    makes createBoard and renderBoard function to dynamically create the hidden codeword and render to the GUI

--- hangman.js
+++ hangman.js
@@ -1,11 +1,52 @@
 console.log('hangman.js here...')
 
 const codeWord = 'coding';
-let board = ['*','*','*','*','*','*'];
+
+//takes a codeword and returns an array the length of the word, where each element in the array is *
+const createBoard = (codeWord) => {
+  const board = [];
+  for(let i = 0 ; i<codeWord.length ; i++) {
+    board[i] = '*'
+  }
+  return board
+}
+
+//creates board using codeWord
+const board = createBoard(codeWord)
+
+//renders the board to the GUI...
+const renderBoard = () => {
+  
+  //targets board section of gui
+  const boardSection = document.querySelector('.board')
+
+  //for each item in board, create an element and place it in board section
+  for (let i = 0 ; i < board.length ; i++) {
+    let newPiece = document.createElement('span');
+    newPiece.classList.add('board__piece');
+    newPiece.textContent = '*';
+    //creates unique data-piece for each piece, corresponding with the spot in the array
+    newPiece.dataset.piece=i;
+    boardSection.appendChild(newPiece)
+  }
+}
+
+renderBoard();
 
 const displayBoard = board.join();
 
 console.log(board);
+
+const renderGuess = () => {
+  let textArea = document.querySelector('#guess-input');
+  let userInput = textArea.value;
+}
+
+//target submit button
+const submit = document.querySelector('#guess-submit');
+
+//When submit button is clicked, render guess
+submit.addEventListener('click', renderGuess);
 
 const guess = (letter) => {
   //if any letter in code word matches letter input,
